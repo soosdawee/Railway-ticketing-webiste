6ee3c6d59c8de60bcf2bc00bbd056b41020c3df4
soosdawee <soosdavid02@gmail.com>
2023-12-26 22:45:31 +0200

    graphql

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import  {ApolloClient, InMemoryCache, ApolloProvider} from "@apollo/client"
 
 import HomePage from "./pages/Homepage.js";
 import MapPage from "./pages/MapPage.js";
 import SiteHeader from "./components/SiteHeader.js";
 
+const client = new ApolloClient ( {
+  uri: 'http://localhost:1337/graphql',
+  cache: new InMemoryCache()
+})
+
 function App() {
   return (
     <Router>
+      <ApolloProvider client={client}>
       <div className="App">
         <SiteHeader />
         <Routes>
-          <Route exact path='/'  element={<HomePage />} />
+          <Route exact path='/' element={<HomePage />} />
           <Route path='/map' element={<MapPage />} />
         </Routes>
       </div>
+      </ApolloProvider>
     </Router>
   );
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,19 +1,56 @@
-import React from 'react'
-import useFetch from '../hooks/useFetch'
+import React, {useState} from 'react'
+import {useQuery, gql} from '@apollo/client'
+
+const CITIES = gql`
+    query GetCities {
+        cities {
+        data {
+            id,
+            attributes {
+            name,
+            body
+            }
+        }
+        }
+    }
+`
+
+const ReadMore = ({children}) => {
+    const text = children;
+    const [isReadMore, setIsReadMore] = useState(true);
+    const toggleReadMore = () => {
+        setIsReadMore(!isReadMore);
+    };
+
+    return (
+        <p className="text">
+            {isReadMore ? text.slice(0, 150) : text}
+            <span
+                onClick={toggleReadMore}
+                className="read-or-hide"
+                style={{ color: "blue" }}
+            >
+                {isReadMore ? " ... Read more" : " Show less"}
+            </span>
+        </p>
+    );
+};
 
 export default function HomePage() {
-    const {loading, error, data} = useFetch('http://localhost:1337/api/cities')
-    if (loading) return <p>Loading...</p>
-    if (error.length > 0) return <p>Error</p>
+    const { loading, error, data } = useQuery(CITIES) 
 
-    console.log(data)
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Error</p>
+
+    console.log(data.cities.data)
 
     return (
       <div>
-          {data.map(city => (
+          {data.cities.data.map(city => (
               <div key={city.id} className='city-card'>
                   <h2>{city.attributes.name}</h2>
-                  <p>{city.attributes.body}</p>
+                  <ReadMore>{city.attributes.body}</ReadMore>
+                  
               </div>
           ))}
       </div>
